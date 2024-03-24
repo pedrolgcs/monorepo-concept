07548d4e9fecf9ee67dfc127696899b293eb27a2
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-03-24 15:35:21 -0300

    chore: configure eslint

--- apps/cars/src/app/page.tsx
+++ apps/cars/src/app/page.tsx
@@ -1,9 +1,7 @@
-import {} from '@repo/ui/components/button';
-
 export default function Home() {
   return (
-    <main className="min-h-screen bg-slate-400 w-full">
-      <div className='h-96 bg-red-300'></div>
+    <main className="min-h-screen w-full bg-slate-400">
+      <div className="h-96 bg-red-300"></div>
     </main>
-  );
+  )
 }
