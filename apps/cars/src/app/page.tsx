a30f18501bf9bd8681067dca373ad44a97ec478e
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-03-29 14:06:54 -0300

    feat: header margin

--- apps/cars/src/app/page.tsx
+++ apps/cars/src/app/page.tsx
@@ -3,7 +3,7 @@ import { ListCars } from '@/features/list-cars'
 export default function Home() {
   return (
     <main className="min-h-screen w-full bg-background">
-      <div className="mx-auto grid w-full max-w-7xl justify-between py-6">
+      <div className="mx-auto grid w-full max-w-7xl justify-between px-4 py-6">
         <ListCars />
       </div>
     </main>
