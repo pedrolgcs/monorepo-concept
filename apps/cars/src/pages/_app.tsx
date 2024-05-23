018da647f500096ca5ef02379a431c64fbeb479d
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-05-23 10:30:36 -0300

    feat: configure msw

--- apps/cars/src/pages/_app.tsx
+++ apps/cars/src/pages/_app.tsx
@@ -7,6 +7,10 @@ import { Header } from '@/features/header'
 import { Theme } from '@/features/theme'
 import { queryClient } from '@/lib/react-query'
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  import('../lib/msw').then(({ initMocks }) => initMocks())
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
